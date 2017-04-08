54c6f7da571e9d87a8f94bed0cd6e6c2fc147952
Juan John-Charles <whoisju1@gmail.com>
2017-04-08 17:44:21 -0400

    issue with where this points to fixed

--- ClozeCard.js
+++ ClozeCard.js
@@ -4,21 +4,19 @@ module.exports = function ClozeCard(text, cloze) {
 
     this.clozeDeleted = function() {
         return this.cloze;
-    }();
+    }.bind(this)();
     this.partial = function() {
 
         var pattern = new RegExp(cloze, "i");
-        //Test to see if the cloze deletion is in the full text
         var result = pattern.test(text);
-        //return truncated string
         if (result) {
             return "..." + text.slice(cloze.length);
         } else {
             throw Error;
         }
-    }();
+    }.bind(this)();
     this.fullText = function() {
-        // return this.text;
-        return text; //when the "this" keyword is placed before "text" the method returnd undefined
-    }();
+
+        return this.text;
+    }.bind(this)();
 };
